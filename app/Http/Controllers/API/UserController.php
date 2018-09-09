0d28236b74f807b3b3a0040f2e28b4e74bc43f4c
Basem <ebasem65@gmail.com>
2018-09-09 19:08:46 +0200

    new

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -80,16 +80,16 @@ class UserController extends Controller
 
         $checkImage = false;
         $user = new User();
-        if($request->hasFile('personalImageUrl'))
-        {
+        //if($request->hasFile('personalImageUrl'))
+        //{
             $image = $request->file('personalImageUrl');
-            $name = str_slug($request->name) . '.' . $image->getClientOriginalExtension();
+            $name = str_slug($request->email) . '.' . $image->getClientOriginalExtension();
             $destinationPath = public_path('/images');
             $imagePath = $destinationPath . '/' . $name;
             $image->move($destinationPath, $name);
             $user->personalImageUrl = $name;
             $checkImage = true;
-        }
+        //}
         $user->name = $request->get('name');
         $user->password = bcrypt($request->get('password'));
         $user->email = $request->get('email');
